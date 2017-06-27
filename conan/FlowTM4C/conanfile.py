81c0d66e616c6a14685a75e6781b07104ac9c666
m-spiessens <mathias@spiessens.eu>
2017-06-27 20:05:05 +0200

    Preparing release 1.1.

--- conan/FlowTM4C/conanfile.py
+++ conan/FlowTM4C/conanfile.py
@@ -5,19 +5,19 @@ import shutil
 
 class FlowTM4C(ConanFile):
 	name = "FlowTM4C"
-	version = "1.0"
+	version = "1.1"
 	description = """Flow compatible drivers for the Texas Intruments TM4C129 microcontroller family."""
 	url = "https://github.com/CynaraKrewe/FlowTM4C"
 	license = "MIT"
 	author = "Mathias Spiessens"
 	build_policy = "missing"
-	requires = "FlowCore/1.1@spiessensm/stable", "TM4C-usblib/2.1.4.178@spiessensm/stable"
+	requires = "FlowCore/1.2@spiessensm/stable", "TM4C-usblib/2.1.4.178@spiessensm/stable"
 	
 	def source(self):
-		download("https://github.com/CynaraKrewe/FlowTM4C/archive/v1.0.zip", "FlowTM4C-1.0.zip")
-		unzip("FlowTM4C-1.0.zip")
-		shutil.move("FlowTM4C-1.0", "FlowTM4C")
-		os.unlink("FlowTM4C-1.0.zip")
+		download("https://github.com/CynaraKrewe/FlowTM4C/archive/v1.1.zip", "FlowTM4C-1.1.zip")
+		unzip("FlowTM4C-1.1.zip")
+		shutil.move("FlowTM4C-1.1", "FlowTM4C")
+		os.unlink("FlowTM4C-1.1.zip")
 
 	def build(self):
 		self.output.info("Nothing to build, this package provides sources.")
